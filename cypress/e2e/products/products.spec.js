5c1d6c2ce04eb6c9802fd1d218bf837b5c82ba80
Anderson Diniz <diniz@mbp-de-diniz.lan>
2023-07-05 16:25:47 -0300

    ajustes

--- cypress/e2e/products/products.spec.js
+++ cypress/e2e/products/products.spec.js
@@ -3,7 +3,23 @@ const products = new Products();
 
 
 describe('GET Products', { tags: ['regression', 'products'] }, () => {
-    it('Products - Get all products', () => {
+    it('Products - Get all products', { tags: ['get-products'] }, () => {
         products.getProducts();
+        products.validateProducts();
+    });
+
+    it('Products - Get single product', { tags: ['get-products'] }, () => {
+        products.getSingleProduct();
+        products.validateProducts();
+    });
+
+    it('Products - Add product', { tags: ['add-product'] }, () => {
+        products.postAddProduct();
+        products.validateProducts();
+    });
+
+    it('Products - Delete product', { tags: ['delete-product'] }, () => {
+        products.deleteProduct();
+        products.validateError();
     });
 })
--- cypress/services/products/products.js
+++ cypress/services/products/products.js
@@ -1,24 +1,5 @@
 
 export class Products {
-    postRegisterIntegrationSuccessWithoutLimit() {
-        const pure = require('pure-gen');
-        const cnpj = pure.document.brazilianCompanyNumber();
-        const email = pure.internet.email();
-        cy.request({
-            method: 'POST',
-            url: `${Cypress.config().baseUrl}/register/integration`,
-            headers: {
-                "Authorization": `Basic ${Cypress.config().basicAuthorization}`
-            },
-            body: {
-                "document": cnpj,
-                "email": email
-            }
-        }).then((res) => {
-            cy.wrap(res).as('response');
-        })
-    }
-
     getProducts() {
         cy.request({
             method: 'GET',
@@ -28,11 +9,61 @@ export class Products {
         })
     }
 
-    validateRegisterWithoutLimit() {
-        cy.get('@response').then((response) => {
-            expect(response.status).to.eq(201);
-            expect(response.body.message).to.eq("success");
-            // expect(response.body.analysis.limit).to.eq("0.000");
+    getSingleProduct() {
+        cy.request({
+            method: 'GET',
+            url: `${Cypress.config().baseUrl}/products/1`,
+        }).then((res) => {
+            cy.wrap(res).as('response');
+        })
+    }
+
+    postAddProduct() {
+        cy.request({
+            method: 'POST',
+            url: `${Cypress.config().baseUrl}/products/add`,
+            body: {
+                    "id": 700,
+                    "title": "iPhone 16",
+                    "description": "An apple mobile which is nothing like apple",
+                    "price": 549,
+                    "discountPercentage": 12.96,
+                    "rating": 4.69,
+                    "stock": 94,
+                    "brand": "Apple",
+                    "category": "smartphones",
+                    "thumbnail": "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+                    "images": [
+                        "https://i.dummyjson.com/data/products/1/1.jpg",
+                        "https://i.dummyjson.com/data/products/1/2.jpg",
+                        "https://i.dummyjson.com/data/products/1/3.jpg",
+                        "https://i.dummyjson.com/data/products/1/4.jpg",
+                        "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+                    ]
+            }
+        }).then((res) => {
+            cy.wrap(res).as('response');
+        })
+    }
+
+    deleteProduct() {
+        cy.request({
+            method: 'DELETE',
+            url: `${Cypress.config().baseUrl}/products/1`,
+        }).then((res) => {
+            cy.wrap(res).as('response');
+        })
+    }
+
+    validateProducts(){
+        cy.get('@response').then((res) => {
+            expect(res.status).to.eq(200)
+        })
+    }
+
+    validateError(){
+        cy.get('@response').then((res) => {
+            expect(res.status).to.eq(204)
         })
     }
 }
